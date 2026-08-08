13877c128d27120c53e50f596b282efcc3b32520
KCC Service Learning Office <kccserve@hawaii.edu>
2026-08-08 09:06:13 -1000

    Add Contact route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import PathwaysOverview from './pages/PathwaysOverview'
+import Contact from './pages/Contact'
 import PathwayDetail from './pages/PathwayDetail'
 import LeafPage from './pages/LeafPage'
 import NotFound from './pages/NotFound'
@@ -53,6 +54,7 @@ export default function App() {
       <div key={pathname} className="page-enter">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/pathways" element={<PathwaysOverview />} />
           <Route path="/pathways/:slug" element={<PathwayDetail />} />
 
